f85643f671f1d5be3dce05cb28167655dc146374
Santiago <98619680+Santygr21@users.noreply.github.com>
2024-06-05 20:49:39 -0500

    VAMOOOOOOOO

--- app.py
+++ app.py
@@ -4,9 +4,15 @@ import pandas as pd
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
 import os
+from flask import send_from_directory
+
 
 app = Flask(__name__)
 CORS(app)
+
+@app.route('/static/<path:path>')
+def serve_static(path):
+    return send_from_directory('static', path)
 
 @app.route('/')
 def index():
@@ -34,15 +40,15 @@ def crear_protopersona(datos, vecinos):
     protopersona = datos.loc[vecinos].mean()
     return protopersona
 
-def calcular_maximo(datos, protopersona):
-    promedios = datos.mean()
-    categorias_maximo = promedios[promedios > protopersona].index.tolist()
-    return categorias_maximo
-
 def calcular_minimo(datos, protopersona):
     valores_minimos = datos.min()
     categorias_minimo = valores_minimos[valores_minimos < protopersona].index.tolist()
     return categorias_minimo
+
+def calcular_maximo(datos, protopersona):
+    promedios = datos.mean()
+    categorias_maximo = promedios[promedios > protopersona].index.tolist()
+    return categorias_maximo
 
 def calcular_promedio(datos, protopersona):
     promedios = datos.mean()
@@ -68,53 +74,47 @@ def obtener_categorias(datos, protopersona, metodo):
     categorias_dict = {categoria: protopersona[categoria] for categoria in categorias}
     return categorias_dict
 
+def obtener_recomendaciones_diferentes_categorias(df_eventos, categorias_cumplen_requisito, N):
+    eventos_recomendados = []
+    for categoria in categorias_cumplen_requisito.keys():
+        eventos_categoria = df_eventos[df_eventos['categoria'] == categoria]
+        if not eventos_categoria.empty:
+            evento_recomendado = eventos_categoria.sample()
+            eventos_recomendados.append(evento_recomendado)
+    
+    # Si no se encontraron eventos para todas las categorías, puedes llenar con eventos adicionales de otras categorías.
+    while len(eventos_recomendados) < N:
+        evento_adicional = df_eventos.sample()
+        eventos_recomendados.append(evento_adicional)
+    
+    return pd.concat(eventos_recomendados).head(N)
+
 @app.route('/user-data', methods=['POST'])
 def create_data():
     data = request.get_json()
-    print(data)
     usuario_referencia = data['usuario']
     k_vecinos = data['vecinos']
     metodo_aggregation = data['aggregation']
     N = data['N']
 
-    
-    
     df_usuarios = importar_base_datos('./dataBases/Base_usuarios.csv')
     if df_usuarios is None:
         return jsonify({'error': 'Base de datos de usuarios no encontrada'}), 400
-    print(df_usuarios)
-    usuarios = df_usuarios.index.tolist()
-    print(usuarios)
 
     resultados_similitud = calcular_similitud_coseno(df_usuarios, usuario_referencia, k_vecinos)
-    print(resultados_similitud)
     vecinos_cercanos = list(resultados_similitud.keys())
 
     protopersona = crear_protopersona(df_usuarios, vecinos_cercanos)
-    print(protopersona)
 
     categorias_cumplen_requisito = obtener_categorias(df_usuarios, protopersona, metodo_aggregation)
-    print(categorias_cumplen_requisito)
 
     df_eventos = importar_base_datos('./dataBases/Base_eventos.csv')
     if df_eventos is None:
         return jsonify({'error': 'Base de datos de eventos no encontrada'}), 400
-    print(df_eventos)
 
-    categorias = list(categorias_cumplen_requisito.keys())
-    eventos_filtrados = df_eventos[df_eventos['categoria'].isin(categorias)]
-    recomendaciones = eventos_filtrados.head(N)
-    print(recomendaciones[['id_evento', 'categoria']])
-    print(recomendaciones.head(10))  # Esto imprimirá las primeras 10 filas
-
-
+    recomendaciones = obtener_recomendaciones_diferentes_categorias(df_eventos, categorias_cumplen_requisito, N)
     arreglo_recomendaciones = recomendaciones[['id_evento', 'categoria', 'fecha', 'descripcion']].values.tolist()
-    print(type(arreglo_recomendaciones), 'Hola')
-   
 
-    if not data:
-        return jsonify({'error': 'No data provided'}), 400
-    
     return jsonify({'resultados': arreglo_recomendaciones, 'vecinos': vecinos_cercanos})
 
 if __name__ == "__main__":
